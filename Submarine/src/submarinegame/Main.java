c775c90edc11ba8ac66b57f3ab7e870d0b87a824
user <user@10.124.32.23>
2019-09-20 12:06:34 +0900

    temp

--- Submarine/src/submarinegame/Main.java
+++ Submarine/src/submarinegame/Main.java
@@ -12,6 +12,26 @@ public class Main {
 		
 		cellBtn = new JButton[15][20];
 		
-		for (int i)
+		for (int i = 0 ; i < 15 ; i++) {
+			
+			for (int j = 0 ; j < 15 ; j++) {
+				
+				JButton btn = new JButton();
+				
+				btn.addActionListener(e -> {
+					
+					check(j,i);
+					
+				});
+				
+			}
+			
+		}
+	}
+
+
+	private void check(int x, int y) {
+		// TODO Auto-generated method stub
+		
 	}
 }
