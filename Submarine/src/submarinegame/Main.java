ab1a409b59e2061cf21fce15a8a2e53cd4f7f3de
awidesky <awidesky@naver.com>
2019-09-20 12:06:08 +0900

    temp

--- Submarine/src/submarinegame/Main.java
+++ Submarine/src/submarinegame/Main.java
@@ -1,16 +1,17 @@
 package submarinegame;
 
-import java.util.ArrayList;
-import enemy.*;
+import javax.swing.JButton;
 
 public class Main {
 	
-	public static ArrayList<String> shipname = new ArrayList<String>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public JButton[][] cellBtn;
+	
 
+	public void newgame(int enemies) {
 		
+		cellBtn = new JButton[15][20];
+		
+		for (int i)
 	}
-
 }
---
+++ Submarine/src/submarinegame/MainFrame.java
@@ -0,0 +1,7 @@
+package submarinegame;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+}
